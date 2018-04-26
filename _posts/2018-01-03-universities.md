120a32db9809710a558518b281410ff434490151
Ivar Lee <ivar@hackerspring.com>
2018-04-26 14:42:58 -0700

    adding scroll script and dynamic carets for opening and closing articles in mobile view

--- _posts/2018-01-03-universities.md
+++ _posts/2018-01-03-universities.md
@@ -2,6 +2,7 @@
 title: Universities
 date: 2018-01-03
 ---
+
 Fill
 
 ---
--- javascript/main.js
+++ javascript/main.js
@@ -1,46 +1,38 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
 
 const footer = document.getElementById('footer')
 const footerHeader = document.getElementsByClassName('header')
 const footerLinks = document.getElementsByClassName('links')
-
-const learnMenuToggle = document.getElementById('learnMenuToggle')
-const developMenuToggle = document.getElementById('developMenuToggle')
-const communityMenuToggle = document.getElementById('communityMenuToggle')
+const footerCarets = document.getElementsByClassName('footer-caret')
+let lastFooterTarget = null
 
 // functions
 
+const setCaretOrientationToClosed = () => {
+	for (keys in footerCarets) {
+		footerCarets[keys].src = '../img/footer/up-caret.svg'
+	}
+}
+
 const open = (target) => {
 
-	const links = target.nextElementSibling
-	const caretDirection = target.childNodes[3].childNodes[0].src.split('/')[5]
-	let targetCaretImg = target.childNodes[3].childNodes[0]
-	
-	switch(caretDirection) {
-		case 'up-caret.svg':
-			learnMenuToggle.src = '/img/footer/up-caret.svg'
-			developMenuToggle.src = '/img/footer/up-caret.svg'
-			communityMenuToggle.src = '/img/footer/up-caret.svg'
-			targetCaretImg.src = '/img/footer/down-caret.svg'
-		break
-		case 'down-caret.svg':
-			learnMenuToggle.src = '/img/footer/up-caret.svg'
-			developMenuToggle.src = '/img/footer/up-caret.svg'
-			communityMenuToggle.src = '/img/footer/up-caret.svg'
-			targetCaretImg.src = '/img/footer/up-caret.svg'
-		default:
-		break
+	if (target === lastFooterTarget) {
+		target.lastElementChild.children[0].src = '/img/footer/up-caret.svg'
 	}
+
+	const links = target.nextElementSibling
 
 	if(links.classList.contains('open')) {
 		return links.classList.remove('open')
 	}
 
-	Array.prototype.forEach.call( footerLinks, function(item){
+	Array.prototype.forEach.call(footerLinks, (item) => {
 		item.classList.remove('open')
 	})
 
+	setCaretOrientationToClosed()
 	links.classList.add('open')
+	target.lastElementChild.children[0].src = '/img/footer/down-caret.svg'
 }
 
 window.scrollToSpot = (elm) => {
@@ -50,15 +42,18 @@ window.scrollToSpot = (elm) => {
 
 // event listeners
 
-footer.addEventListener('click', function(e) {
+footer.addEventListener('click', (e) => {
 
 	const target = e.target
 
 	if (target.classList.contains('header')) {
-		open( target )
-	} else if ( target.parentElement.classList.contains('header')) {
+		open(target)
+		lastFooterTarget = target
+	} else if (target.parentElement.classList.contains('header')) {
 		open(target.parentElement)
+		lastFooterTarget = target.parentElement
 	}
+
 
 })
 
